e59f8115c0a7b693d712f081acae950baee6c01c
Sjors Robroek <s.robroek@vxsan.com>
2020-11-19 16:01:08 +0400

    remove submodules

--- modules/policy_group/vrlcm/output.tf
+++
@@ -1,8 +0,0 @@
-output "groups" {
-  value = {
-    # provider = nsxt_policy_group.application_providers
-    app = nsxt_policy_group.application
-    # intra-app = nsxt_policy_group.intra-app_providers
-
-  }    
-}
--- modules/policy_group/ws1a-reg/output.tf
+++
@@ -1,9 +0,0 @@
-output "groups" {
-  value = {
-    # provider = nsxt_policy_group.application_providers
-    app = nsxt_policy_group.application
-    lb = nsxt_policy_group.loadbalancer
-    # intra-app = nsxt_policy_group.intra-app_providers
-
-  }    
-}
